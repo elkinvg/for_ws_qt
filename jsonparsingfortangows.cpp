4237178ddb2f4c414c753d82f342b73262c8d8a9
V.G.Elkin <elkinvg@gmail.com>
2016-12-12 09:53:24 +0300

    test json

--- jsonparsingfortangows.cpp
+++ jsonparsingfortangows.cpp
@@ -1,8 +1,36 @@
 #include "jsonparsingfortangows.h"
 
 #include "common_ws.h"
+#include <QJsonObject>
+#include <QJsonDocument>
+
+#include <iostream>
+using std::cout;
+using std::endl;
 
 JsonParsingForTangoWs::JsonParsingForTangoWs()
 {
 
+}
+
+TangoDataFromJson* JsonParsingForTangoWs::parseJson(const QString &json)
+{
+    QJsonDocument jsondoc = QJsonDocument::fromJson(json.toUtf8());
+
+    if (jsondoc.isNull())
+        return nullptr;
+
+    if(jsondoc.isObject()) {
+        cout << "jsondoc.isObject()" << endl;
+        QJsonObject jsonObj = jsondoc.object();
+        TypeReq typeReq;
+        if(jsonObj.find("type_req")) {
+            if(jsonObj["type_req"] == "attribute")
+                typeReq = TypeReq::ATTRIBUTE;
+            if(jsonObj["type_req"] == "command")
+                typeReq = TypeReq::COMMAND;
+        }
+    }
+
+    return nullptr;
 }
--- jsonparsingfortangows.h
+++ jsonparsingfortangows.h
@@ -1,11 +1,14 @@
 #ifndef JSONPARSINGFORTANGOWS_H
 #define JSONPARSINGFORTANGOWS_H
 
+#include <QString>
+#include <tangodatafromjson.h>
 
 class JsonParsingForTangoWs
 {
 public:
     JsonParsingForTangoWs();
+    TangoDataFromJson *parseJson(const QString& json);
 };
 
 #endif // JSONPARSINGFORTANGOWS_H
